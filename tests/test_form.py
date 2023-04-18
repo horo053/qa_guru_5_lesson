9d89b312aa57230fa3c8207d667c9b14137a5528
Streshneva Kristina <strashneva@proitr.ru>
2023-04-18 13:38:37 +0300

    Исправление поиска радио баттона Пол

--- tests/test_form.py
+++ tests/test_form.py
@@ -7,14 +7,14 @@ def test_form():
     browser.open('/automation-practice-form')
 
     #заполнение полей: Name, Email, Gender, Mobile
-    browser.element('[id = firstName]').type('Ivan')
-    browser.element('[id = lastName]').type('Ivanovich')
-    browser.element('[id = userEmail]').type('ivantest@mail.ru')
-    browser.element('[name = gender][value = Male]').double_click()
-    browser.element('[id= userNumber]').set('8999777666')
+    browser.element('[id=firstName]').type('Ivan')
+    browser.element('[id=lastName]').type('Ivanovich')
+    browser.element('[id=userEmail]').type('ivantest@mail.ru')
+    browser.element('[for=gender-radio-1]').click()
+    browser.element('[id=userNumber]').set('8999777666')
 
     #Выбор даты рождения - Date of Birth
-    browser.element('[id = dateOfBirthInput]').click()
+    browser.element('[id=dateOfBirthInput]').click()
     browser.element('.react-datepicker__month-select').click()
     browser.element('.react-datepicker__month-select').element('[value = "9"]').click()
     browser.element('.react-datepicker__year-select').click()
@@ -22,20 +22,20 @@ def test_form():
     browser.element('.react-datepicker__day--013').click()
 
     #заполнение полей: Subjects, Hobbies
-    browser.element('[id = subjectsInput]').type('Arts').press_enter()
-    browser.element('[for = hobbies-checkbox-3]').click()
+    browser.element('[id=subjectsInput]').type('Arts').press_enter()
+    browser.element('[for=hobbies-checkbox-3]').click()
 
     #загрузка файла - Picture
     #ответ найден на https://software-testing.ru/forum/index.php?/topic/33954-selenium-zagruzka-fajla-cherez-knopku-zagruzi/
-    browser.element('[id = uploadPicture]').send_keys(os.getcwd() + '/test.png')
+    browser.element('[id=uploadPicture]').send_keys(os.getcwd() + '/test.png')
 
     #заполнение полей: Current Address, State and City
-    browser.element('[id = currentAddress]').type('Moscow')
-    browser.element('[id = react-select-3-input]').type('Haryana').press_enter()
-    browser.element('[id = react-select-4-input]').type('Panipat').press_enter()
+    browser.element('[id=currentAddress]').type('Moscow')
+    browser.element('[id=react-select-3-input]').type('Haryana').press_enter()
+    browser.element('[id=react-select-4-input]').type('Panipat').press_enter()
 
     #нажатие на кнопку - Submit
-    browser.element('[id = submit]').press_enter()
+    browser.element('[id=submit]').press_enter()
 
     #проверка результирующей таблицы
     browser.all('tbody tr').should(have.exact_texts(
